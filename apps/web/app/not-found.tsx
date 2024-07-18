bc20a0a5de4d5f5cb3cde814c779e751480570b4
Amir Ali <amiraliazimloo123@gmail.com>
2024-07-19 00:22:25 +0330

    Add the NotFound page.

---
+++ apps/web/app/not-found.tsx
@@ -0,0 +1,10 @@
+
+const NotFound = () => {
+   return (
+      <div className="flex h-[20rem] flex-col items-center justify-between p-24 gap-4">
+         <div className=" text-2xl">page not found</div>
+      </div>
+   );
+}
+
+export default NotFound;
