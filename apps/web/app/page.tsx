9d2509d48a31a1d629d05c30c06890e138949cab
Amir Ali <amiraliazimloo123@gmail.com>
2024-07-18 22:26:25 +0330

    Add the Schema Structure.

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,17 +3,98 @@ import { Button } from "@repo/ui/button";
 import styles from "./page.module.css";
 
 export default function Home() {
+  function addSiteJsonLd() {
+    return {
+      __html: `
+       {
+          "@context": "http://schema.org",
+          "@graph": [
+             {
+                "@type": "Organization",
+                "@id": "https://url/#organization",
+                "name": "Next js",
+                "url": "https://url/",
+                "sameAs": [
+                   "https://twitter.com/account",
+                   "https://www.linkedin.com/in/account",
+                   "https://www.instagram.com/account"
+                ],
+                "logo": {
+                   "@type": "ImageObject",
+                   "@id": "https://url/#logo",
+                   "url": "https://url/logo.png",
+                   "width": 350,
+                   "height": 350,
+                   "caption": "کپشن"
+                }}]}
+       `,
+    };
+  }
+
+  function addBreadCrumbsJsonLd() {
+    return {
+      __html: `
+       {
+          "@context": "https://schema.org",
+          "@type": "BreadcrumbList",
+          "itemListElement":
+          [
+           {
+            "@type": "ListItem",
+            "position": 1,
+            "item":
+            {
+             "@id": "https://url",
+             "name": "خانه"
+             }
+           },
+           {
+            "@type": "ListItem",
+           "position": 2,
+           "item":
+            {
+              "@id": "https://url/nextjs",
+              "name": "Next js"
+            }
+           }
+          ]
+         }
+       `,
+    };
+  }
+
+  function addProductJsonLd() {
+    return {
+      __html: `
+       {
+          "@context": "https://schema.org",
+          "@type": "Product",
+          "aggregateRating": {
+            "@type": "AggregateRating",
+            "ratingValue": "4.25",
+            "reviewCount": "10"
+          },
+          "name": "product name",
+          "description": "this is page discription",
+          "image": "product image url",
+          "offers": {
+            "@type": "Offer",
+            "availability": "https://schema.org/InStock",
+            "price": "5000",
+            "priceCurrency": "IRR"
+          }
+       }
+       `,
+    };
+  }
+
   return (
     <>
       {/* SEO Meta Tags */}
       <>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link
-          rel="shortcut icon"
-          href="/favicon.ico"
-          type="image/x-icon"
-        />
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
 
         <meta name="robots" content="index, follow" />
         <link rel="canonical" href="https://url" />
@@ -28,45 +109,58 @@ export default function Home() {
 
         <meta name="twitter:title" content="this is page title" />
         <meta name="twitter:description" content="this is page discrption" />
-        <meta
-          name="twitter:image"
-          content="https://url.svg"
-        />
+        <meta name="twitter:image" content="https://url.svg" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="twitter-site" />
+
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={addSiteJsonLd()}
+          key="site-jsonld"
+        />
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={addBreadCrumbsJsonLd()}
+          key="breadcrumbs-jsonld"
+        />
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={addProductJsonLd()}
+          key="product-jsonld"
+        />
       </>
 
-         {/* HTML 5 Structure */}
-         <div>
-            <main>
-               <h1></h1>
-               <section>
-                  <header>
-                     <h2></h2>
-                  </header>
-                  <p></p>
-                  <h3></h3>
-                  <img />
-                  <p></p>
-                  <h3></h3>
-                  <p></p>
-                  <ul></ul>
-                  <p></p>
-                  <div>
-                     <article></article>
-                     <article></article>
-                     <article></article>
-                     <article></article>
-                  </div>
-               </section>
-            </main>
-            <aside>
-               <article></article>
-               <article></article>
-               <article></article>
-               <article></article>
-            </aside>
-         </div>
+      {/* HTML 5 Structure */}
+      <div>
+        <main>
+          <h1></h1>
+          <section>
+            <header>
+              <h2></h2>
+            </header>
+            <p></p>
+            <h3></h3>
+            <img />
+            <p></p>
+            <h3></h3>
+            <p></p>
+            <ul></ul>
+            <p></p>
+            <div>
+              <article></article>
+              <article></article>
+              <article></article>
+              <article></article>
+            </div>
+          </section>
+        </main>
+        <aside>
+          <article></article>
+          <article></article>
+          <article></article>
+          <article></article>
+        </aside>
+      </div>
 
       <div className={styles.page}>
         <div className="!bg-green-500 !text-red-500">Test Tailwind Config</div>
